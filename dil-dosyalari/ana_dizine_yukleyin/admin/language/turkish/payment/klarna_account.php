48adc6a3fb813c71a2522dff1b5e2b35683d5d70
Erdem Corapcioglu <erdem.corapcioglu@gmail.com>
2013-01-12 20:46:46 +0200

    Pull from master

---
+++ dil-dosyalari/ana_dizine_yukleyin/admin/language/turkish/payment/klarna_account.php
@@ -0,0 +1,53 @@
+<?php
+/* Turkceye Ceviren eka7a - http://www.e-piksel.com */
+
+// Heading
+$_['heading_title'] = 'Klarna Account';
+
+$_['text_klarna_account'] = '<a onclick="window.open(\'https://merchants.klarna.com/signup?locale=en&partner_id=d5c87110cebc383a826364769047042e777da5e8&utm_campaign=Platform&utm_medium=Partners&utm_source=Opencart\');"><img src="https://cdn.klarna.com/public/images/global/logos/v1/basic/global_basic_logo_std_blue-black.png?width=60&eid=opencart" alt="Klarna Account" title="Klarna Account" style="border: 1px solid #EEEEEE;" /></a>';
+
+// Text
+$_['text_payment']		 = 'Ödeme Metodları';
+$_['text_success']		 = 'Başarılı: Klarna Part Ödeme modülü güncellendi!';
+$_['text_log_clear'] = 'Success: Log was cleared';
+$_['text_live'] = 'Live';
+$_['text_beta'] = 'Beta';
+$_['text_settings'] = 'Settings';
+$_['text_log'] = 'Logs';
+$_['text_help'] = 'Help';
+$_['text_enabled'] = 'Enabled';
+$_['text_disabled'] = 'Disabled';
+$_['text_all_zones'] = 'All Zones';
+
+$_['text_sweden'] = 'Sweden';
+$_['text_norway'] = 'Norway';
+$_['text_finland'] = 'Finland';
+$_['text_denmark'] = 'Denmark';
+$_['text_germany'] = 'Germany';
+$_['text_netherlands'] = 'The Netherlands';
+
+$_['text_clear'] = 'Clear';
+
+// Entry
+$_['entry_merchant'] = 'Klarna Merchant ID:';
+$_['entry_secret'] = 'Klarna Secret:';
+$_['entry_server'] = 'Server:';
+$_['entry_pending_order_status'] = 'Pending Order Status:';
+$_['entry_accepted_order_status'] = 'Accepted Order Status:';
+$_['entry_geo_zone'] = 'Geo Zone:';
+$_['entry_status'] = 'Status:';
+$_['entry_sort_order'] = 'Sort Order:';
+$_['entry_minimum_amount'] = 'Minimum Amount:';
+
+// Help
+$_['help_minimum_total'] = "Cart's total before the module becomes active";
+$_['help_merchant'] = '(estore id) to use for the Klarna service (provided by Klarna).';
+$_['help_secret'] = 'Shared secret to use with the Klarna service (provided by Klarna).';
+
+// Error
+$_['error_permission'] = 'Warning: You do not have permission to modify payment Klarna Part Payment!';
+$_['error_retrieve_pclass'] = 'Could not retrieve pClass for %s. Error Code: %s; Error Message: %s';
+$_['error_log_clear'] = 'Error: Log file was not cleared. Please check write permissions for the file';
+$_['error_http_error'] = 'HTTP Error - Code: %d; Message: %s';
+$_['error_update'] = 'There were errors updating the module. Please check the log file.';
+?>
