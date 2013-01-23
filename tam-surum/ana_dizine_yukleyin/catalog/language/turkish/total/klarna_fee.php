94baa3a3c806997fcd2f6947f06f4aed1d8e620b
Erdem Corapcioglu <erdem.corapcioglu@gmail.com>
2013-01-23 16:48:03 +0200

    Pull from master + simplify structure (only language files)

--- tam-surum/ana_dizine_yukleyin/catalog/language/turkish/total/klarna_fee.php
+++
@@ -1,5 +0,0 @@
-<?php
-/* Turkceye Ceviren eka7a - http://www.e-piksel.com */
-
-$_['text_klarna_fee'] = 'Klarna Ücreti';
-?>
--- tam-surum/ana_dizine_yukleyin/catalog/language/turkish/total/low_order_fee.php
+++
@@ -1,5 +0,0 @@
-<?php
-/* Turkceye Ceviren eka7a - http://www.e-piksel.com */
-
-$_['text_low_order_fee'] = 'Düşük Sipariş Fiyatı';
-?>
